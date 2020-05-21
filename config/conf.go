6d4f309d4a776bd4f49f05595be2d5237cd239ca
zlyuancn <zlyuancn@163.com>
2020-05-21 15:18:25 +0800

    环境变量解析包替换

--- config/conf.go
+++ config/conf.go
@@ -9,7 +9,7 @@
 package config
 
 import (
-    "github.com/kelseyhightower/envconfig"
+    "github.com/zlyuancn/zenvconf"
     "github.com/zlyuancn/zlog"
 )
 
@@ -19,28 +19,28 @@ const (
 )
 
 var Config struct {
-    Bind   string `envconfig:"DRONE_BIND"`   // bind端口
-    Debug  bool   `envconfig:"DRONE_DEBUG"`  // debug模式
-    Secret string `envconfig:"DRONE_SECRET"` // webhook秘钥
+    Bind   string `env:"DRONE_BIND"`   // bind端口
+    Debug  bool   `env:"DRONE_DEBUG"`  // debug模式
+    Secret string `env:"DRONE_SECRET"` // webhook秘钥
 
-    DroneServer string `envconfig:"DRONE_SERVER"` // drone服务地址
+    DroneServer string `env:"DRONE_SERVER"` // drone服务地址
 
-    LogPath string `envconfig:"LOG_PATH"` // 日志路径
+    LogPath string `env:"LOG_PATH"` // 日志路径
 
-    Notifer         string `envconfig:"NOTIFER"`           // 通告者,多个通告者用半角逗号隔开
-    NotifyRetry     int    `envconfig:"NOTIFY_RETRY"`      // 通告失败重试次数
-    OffCreateNotify bool   `envconfig:"OFF_CREATE_NOTIFY"` // 关闭创建动作的通告
+    Notifer         string `env:"NOTIFER"`           // 通告者,多个通告者用半角逗号隔开
+    NotifyRetry     int    `env:"NOTIFY_RETRY"`      // 通告失败重试次数
+    OffCreateNotify bool   `env:"OFF_CREATE_NOTIFY"` // 关闭创建动作的通告
 
-    DingtalkAccessToken     string `envconfig:"DINGTALK_ACCESSTOKEN"`    // 钉钉access_token
-    DingtalkSecret          string `envconfig:"DINGTALK_SECRET"`         // 钉钉secret
-    DingtalkStartTemplate   string `envconfig:"DINGTALK_START_TEMPLATE"` // 钉钉消息任务开始模板文件
-    DingtalkEndTemplateFile string `envconfig:"DINGTALK_END_TEMPLATE"`   // 钉钉消息任务结束模板文件
+    DingtalkAccessToken     string `env:"DINGTALK_ACCESSTOKEN"`    // 钉钉access_token
+    DingtalkSecret          string `env:"DINGTALK_SECRET"`         // 钉钉secret
+    DingtalkStartTemplate   string `env:"DINGTALK_START_TEMPLATE"` // 钉钉消息任务开始模板文件
+    DingtalkEndTemplateFile string `env:"DINGTALK_END_TEMPLATE"`   // 钉钉消息任务结束模板文件
 }
 
 func Init() {
     Config.NotifyRetry = 2
 
-    err := envconfig.Process("", &Config)
+    err := zenvconf.NewEnvConf().Parse(&Config)
     if err != nil {
         zlog.Fatal("初始化失败", err)
     }
--- go.mod
+++ go.mod
@@ -5,9 +5,9 @@ go 1.12
 require (
 	github.com/drone/drone-go v1.0.6
 	github.com/joho/godotenv v1.3.0
-	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/zlyuancn/zdingtalk v0.0.0-20200518042531-89532d56399f
+	github.com/zlyuancn/zenvconf v0.0.0-20200521064154-7b5e8b6a8771
 	github.com/zlyuancn/zlog v0.0.0-20200510075342-5831b7d2f6c6
 	github.com/zlyuancn/zsignal v0.0.0-20200102070656-631fe600ecd4
 )
